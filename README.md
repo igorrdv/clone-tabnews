0b07f722e162dd9b656b9ad6ac8b699fd4cf940a
igorrdv <111236804+igorrdv@users.noreply.github.com>
2023-07-01 00:06:10 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de estudo de como criar um sistema de ponta a ponta.
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-function Home() { 
-    return <h1>You Just Got Rick Rolled</h1>
+function Home() {
+  return <h1>You Just Got Rick Rolled</h1>;
 }
 export default Home;
-
-
-""
