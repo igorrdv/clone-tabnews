4d235ca58db84fed97771180d6fa78e77bd6313c
igorrdv <111236804+igorrdv@users.noreply.github.com>
2023-08-07 14:22:29 +0000

    Atualização página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
 function Home() {
-  return <h1>You Just Got Rick Rolled</h1>;
+  return (
+    <div>
+      <h1 style={{ wordBreak: "break-word" }}>
+        Dyeni, gosto muito do seu sorriso e da sua pessoinha. Se gosta de mim,
+        dê uma risadinha! 😎
+      </h1>
+      <p
+        style={{
+          opacity: "0.1",
+          position: "absolute",
+          bottom: "0",
+          margin: "20px",
+        }}
+      >
+        Amo seu sorriso, é sério.
+      </p>
+    </div>
+  );
 }
 export default Home;
